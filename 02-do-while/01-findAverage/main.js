724c7450fc5439d780d67a3bbc2ab7b5a3944e9b
ParitaSriwasut <parita.sri0327@gmail.com>
2023-08-10 00:29:26 +0700

    addjust 01-findAverage

--- 02-do-while/01-findAverage/main.js
+++ 02-do-while/01-findAverage/main.js
@@ -1,18 +1,17 @@
 let input;
 let count = 0;
-let avg = 0;
 let sum = 0;
+let average;
 
 do {
-  input = prompt("Please enter any number...");
-  input = parseInt(input);
-  if (input === null || input === 0 || input === -1) {
-    break;
+  input = +prompt("Please enter any number...");
+  if (input === null || isNaN(Number) || input <= 0) {
+    count - 1;
   }
-  sum = sum + input;
   count++;
-  avg = sum / count;
-} while (!input === null || !input === 0 || !input === -1);
-console.log(`count : ${count} `);
-console.log(`sum : ${sum} `);
-console.log(`avg : ${avg} `);
+  sum += input;
+  average = sum / count;
+} while (!input === null || !isNaN(Number) || !input <= 0);
+console.log(`Count : ${count} `);
+console.log(`Sum : ${sum} `);
+console.log(`Avg : ${average} `);
